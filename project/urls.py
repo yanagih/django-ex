3c48a734eed9f637f6afb572df84bd4f91a6dc3d
mu <61280283+mu-perori@users.noreply.github.com>
2023-08-31 11:04:41 +0900

    Add index_ja.html

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, health
+from welcome.views import index, index_ja, health
 
 urlpatterns = [
     # Examples:
@@ -10,6 +10,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', index),
+    url(r'^ja$', index_ja),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
 ]
--- welcome/views.py
+++ welcome/views.py
@@ -19,6 +19,17 @@ def index(request):
         'count': PageView.objects.count()
     })
 
+def index_ja(request):
+    """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
+    hostname = os.getenv('HOSTNAME', 'unknown')
+    PageView.objects.create(hostname=hostname)
+
+    return render(request, 'welcome/index_ja.html', {
+        'hostname': hostname,
+        'database': database.info(),
+        'count': PageView.objects.count()
+    })
+
 def health(request):
     """Takes an request as a parameter and gives the count of pageview objects as reponse"""
     return HttpResponse(PageView.objects.count())

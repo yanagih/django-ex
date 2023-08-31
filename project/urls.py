f8b696d75a35767791429f1b43936954444ab1e0
mu <61280283+mu-perori@users.noreply.github.com>
2023-08-31 15:00:31 +0900

    Update

--- project/urls.py
+++ project/urls.py
@@ -2,14 +2,18 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from welcome.views import index, index_ja, health
+from welcome.views import index, index_ja, health, input_test
+
+# welcomeフォルダの下を一番最初に探してくださいという指示
+app_name = 'welcome'
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', index),
+    url(r'^$', input_test, name='input_test'),
+    url(r'^en$', index),
     url(r'^ja$', index_ja),
     url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
--- welcome/views.py
+++ welcome/views.py
@@ -7,6 +7,30 @@ from . import database
 from .models import PageView
 
 # Create your views here.
+def input_test(request):
+    """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
+    hostname = os.getenv('HOSTNAME', 'unknown')
+    PageView.objects.create(hostname=hostname)
+    
+    return render(request, 'welcome/input_test.html')
+
+def get_nyuuryoku(request):
+    message = 'データ受け取ったよ！'
+    if request.method == 'POST':
+        nyuryoku1 = request.POST['nyuryoku1']
+        nyuryoku2 = request.POST['nyuryoku2']
+        nyuryoku3 = request.POST['nyuryoku3']
+        
+        print("Hello!")
+        params = {
+            "nyuryoku1": nyuryoku1,
+            "nyuryoku2": nyuryoku2,
+            "nyuryoku3": nyuryoku3,
+            "message": message,
+        }
+
+        return render(request, '~', params)
+
 
 def index(request):
     """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""

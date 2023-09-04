46299c328dc33bfe50973b70e146fdd1bce9e7dd
mu <61280283+mu-perori@users.noreply.github.com>
2023-09-04 10:49:58 +0900

    Delete index_ja.html and input_test.html

--- welcome/views.py
+++ welcome/views.py
@@ -8,42 +8,12 @@ from .models import PageView
 from .forms import TestForm
 
 # Create your views here.
-def input_test(request):
-    """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-    
-    my_dict = {
-        'insert_something': '表示テスト',
-        'form': TestForm(),
-        'insert_forms': '初期値', 
-    }
-
-    if (request.method == 'POST'):
-        my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '\n整数型:' + request.POST['num']
-        my_dict['form'] = TestForm(request.POST)
-
-    return render(request, 'welcome/input_test.html', my_dict)
-
-
-
 def index(request):
     """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
     hostname = os.getenv('HOSTNAME', 'unknown')
     PageView.objects.create(hostname=hostname)
 
     return render(request, 'welcome/index.html', {
-        'hostname': hostname,
-        'database': database.info(),
-        'count': PageView.objects.count()
-    })
-
-def index_ja(request):
-    """Takes an request object as a parameter and creates an pageview object then responds by rendering the index view."""
-    hostname = os.getenv('HOSTNAME', 'unknown')
-    PageView.objects.create(hostname=hostname)
-
-    return render(request, 'welcome/index_ja.html', {
         'hostname': hostname,
         'database': database.info(),
         'count': PageView.objects.count()
